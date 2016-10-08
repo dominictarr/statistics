c689a248bccc795fd7ca5f1e88dddffdc414bd9d
Dominic Tarr <dominic.tarr@gmail.com>
2016-10-08 22:31:13 +1300

    test as reduce

--- test/index.js
+++ test/index.js
@@ -1,36 +1,23 @@
 
+
+var reduce = require('../')
+
 var tape = require('tape')
-  , Stats = require ('../')
 
-tape('mean', function (t) {
+tape('statistics as reduce', function (t) {
 
-  var s = Stats()
-  s.value(1).value(2)
-  t.equal(s.mean, 1.5)
-  t.equal(s.min, 1)
-  t.equal(s.max, 2)
+  var stats = [1,2,3].reduce(reduce)
+
+  t.deepEqual(stats, {
+    mean: 2,
+    stdev: Math.sqrt((1*1+2*2+3*3)/3 - 2*2),
+
+    sum: 1+2+3,
+    count: 3,
+    sqsum: 1*1+2*2+3*3
+  })
+
+  console.log(stats)
+
   t.end()
-
 })
-
-tape('stdev', function (t) {
-
-  var s = Stats()
-  s.value(1).value(2).value(3)
-  t.equal(s.mean, 2)
-  t.equal(s.min, 1)
-  t.equal(s.max, 3)
-  //sum of squares
-  t.equal(s.sqsum, 1*1+2*2+3*3)
-  //calculate stdev: (sqsum/n - mean^2)
-
-  var stdev = Math.sqrt((1*1+2*2+3*3)/3 - 2*2)
-  t.equal(s.stdev, stdev)
-
-  t.deepEqual(s.toJSON(), {mean: 2, count: 3, stdev: stdev})
-  t.end()
-
-})
-
-
-
