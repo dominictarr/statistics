9083f3cd48390f49d9239c5138fd75b659e32815
Dominic Tarr <dominic.tarr@gmail.com>
2016-10-10 11:43:01 +1300

    tests for mutate

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 
 
 var reduce = require('../')
+var reduceMutate = require('../mutate')
 
 var tape = require('tape')
 
@@ -21,3 +22,24 @@ tape('statistics as reduce', function (t) {
 
   t.end()
 })
+
+tape('statistics as reduce, with mutations', function (t) {
+
+  var stats = [1,2,3].reduce(reduceMutate)
+
+  t.deepEqual(stats, {
+    mean: 2,
+    stdev: Math.sqrt((1*1+2*2+3*3)/3 - 2*2),
+
+    sum: 1+2+3,
+    count: 3,
+    sqsum: 1*1+2*2+3*3
+  })
+
+  console.log(stats)
+
+  t.end()
+})
+
+
+
