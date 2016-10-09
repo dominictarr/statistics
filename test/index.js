d7fb780fb839215af69eb39e7eb507c03c08443c
Dominic Tarr <dominic.tarr@gmail.com>
2016-10-10 11:55:09 +1300

    test as mutated reference

--- test/index.js
+++ test/index.js
@@ -42,4 +42,28 @@ tape('statistics as reduce, with mutations', function (t) {
 })
 
 
+tape('statistics as reduce, with mutations and references', function (t) {
+
+  var init = reduceMutate.initial()
+
+  var stats = [1,2,3].reduce(reduceMutate, init)
+
+  t.deepEqual(stats, init)
+
+  t.deepEqual(stats, {
+    mean: 2,
+    stdev: Math.sqrt((1*1+2*2+3*3)/3 - 2*2),
+
+    sum: 1+2+3,
+    count: 3,
+    sqsum: 1*1+2*2+3*3
+  })
+
+  t.strictEqual(stats, init)
+  console.log(stats)
+
+  t.end()
+})
+
+
 
